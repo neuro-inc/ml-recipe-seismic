dc80e886c1416f19b93eb66a5de4d5151b72acb6
alexander-rakhlin <rakhlin@gmx.net>
2020-06-09 12:10:13 +0300

    download link

--- download_data.sh
+++ download_data.sh
@@ -3,6 +3,6 @@
 DEST=/.
 TMP=$(mktemp -d)
 
-wget http://data.neu.ro/ml-recipe-seismic.zip -O $TMP/ml-recipe-seismic.zip
+wget https://data.neu.ro/ml-recipe-seismic.zip -O $TMP/ml-recipe-seismic.zip
 unzip $TMP/ml-recipe-seismic.zip -d $DEST
 
