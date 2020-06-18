42b7d2f0271ad6571adb54a521cd7d3bb3a94e80
alexander-rakhlin <rakhlin@gmx.net>
2020-06-18 19:20:13 +0300

    requirements

--- src/download_data.sh
+++ src/download_data.sh
@@ -3,4 +3,4 @@
 DATA_PATH=${DATA_PATH:-/storage_path}
 TMP=$(mktemp -d)
 [ ! -f $TMP/ml-recipe-seismic.zip ] && wget http://data.neu.ro/ml-recipe-seismic.zip -O $TMP/ml-recipe-seismic.zip
-unzip $TMP/ml-recipe-seismic.zip -d $DATA_PATH
+unzip -o $TMP/ml-recipe-seismic.zip -d $DATA_PATH
